bad29adb1b84c8a366479a97965baa4b3bfa4d84
Anthony Tedesco <amtedesco224@yahoo.com>
2022-09-16 16:41:12 -0500

    updated notes and timer

--- mapanimation.js
+++ mapanimation.js
@@ -1,28 +1,30 @@
 // This app tracks a single Boston bus route in real time from Bostons 
 // MTBA api data and then displays the bus location as a marker on the map and 
-// updates every 15 seconds
-
+// updates every 10 seconds
+//////////////////////////////////////////////////////////////////////////////
 
 // This array contains past and current locations of the bus
 const busLocation = [];
+// counter here represents the index of the current bus location
 let counter = 0;
+// this represents the map marker when updated and is initalized to null
 let marker = null;
+
 
 // Get longitude/latitude data from getBusLocation() and push to busLocation array
 async function run() {
   const locations = await getBusLocations();
   console.log(new Date());
-  const lon = parseFloat(JSON.stringify(data[0].attributes.longitude));
-  const lat = parseFloat(JSON.stringify(data[0].attributes.latitude));
+  // Change the number value of data[x] in the "lon" and "lat" 
+  // variables below to see different bus routes
+  const lon = parseFloat(JSON.stringify(data[5].attributes.longitude));
+  const lat = parseFloat(JSON.stringify(data[5].attributes.latitude));
   busLocation.push([lon, lat]);
-  console.log(lon);
-  console.log(lat);
-  console.log(busLocation);
   createMarker();
   move();
 }
 
-// Request bus data from MBTA
+// Request bus data from MBTA and return "data" value back to the run() function
 async function getBusLocations() {
   const url = "https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip";
   const response = await fetch(url);
@@ -44,20 +46,38 @@ let map = new mapboxgl.Map({
 // Adds a marker to the map at the first iteration of busLocation array
 function createMarker() {
   if (counter === 0) {
+    map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: busLocation[counter],
+      zoom: 17,
+    });
     marker = new mapboxgl.Marker()
       .setLngLat(busLocation[counter])
       .addTo(map);
+
   }
 }
 
-// counter here represents the index of the current bus location
 async function move() {
+
+  ////////////////////////////////////////////////////////////////////////
+  // This timer was a fix but not a very desireable one, for some reason 
+  // my marker doesn't update it's location at the first iteration of the 
+  // move() function so I used this timer to make it move every 10 seconds 
+  // consistantly from the start
+  let timer = 5000;
+  if (counter >= 2) timer = 10000;
+  // //////////////////////////////////////////////////////////////////////
+
   setTimeout(() => {
     if (counter >= busLocation.length) return;
     marker.setLngLat(busLocation[counter]);
     counter++;
+    console.log(counter);
     run();
-  }, 15000)
+  }, timer)
 }
+
 
 
